dbfe9c8a40c09501376924f495f06de66c6e2933
xapqrt <fowlstar1@gmail.com>
2025-10-02 11:50:51 +0000

    added basic functionality, a coin collector a simple page and a coin manager

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// basic vite config, will add more later
+export default defineConfig({
+  plugins: [react()],
+});
